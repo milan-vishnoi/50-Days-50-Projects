4b52a7f90b9d0a7d04bbddb311537b86c974064d
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-05 19:29:30 +0530

    Added transition delay for each span of label

--- 8. Form Wave Animation/script.js
+++ 8. Form Wave Animation/script.js
@@ -1,5 +1,7 @@
 const labels = document.querySelectorAll(".form-control label");
 
 labels.forEach(label => { 
-    label.innerHTML = label.innerText.split('').map((letter,index)=>`<span>${letter}</span>`).join('');
+    label.innerHTML = label.innerText.split('')
+                      .map((letter,index)=>`<span style="transition-delay:${index*75}ms">${letter}</span>`)
+                      .join('');
 });
