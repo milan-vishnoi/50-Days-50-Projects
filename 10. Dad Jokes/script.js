b0d87b7d546a3189afda3f1e7e623e0209859ce2
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-07 20:17:54 +0530

    Added generateJoke function using asyn-await to enhance readability.

--- 10. Dad Jokes/script.js
+++ 10. Dad Jokes/script.js
@@ -4,14 +4,27 @@ const jokeButton = document.getElementById("jokesBtn");
 jokeButton.addEventListener("click",generateJoke);
 generateJoke();
 
-function generateJoke() {
-    const config = {
-        headers : {
-            "Accept":"application/json"
-        }
-    };
-    fetch("https://icanhazdadjoke.com",config).then(response => response.json())
-    .then(data => {
+//Approach 1 : Using .then()
+// function generateJoke() {
+//     const config = {
+//         headers : {
+//             "Accept":"application/json"
+//         }
+//     };
+//     fetch("https://icanhazdadjoke.com",config).then(response => response.json())
+//     .then(data => {
+//         jokeElement.innerHTML = data.joke;
+//     } );
+// }
+
+//Approach 2 : Using async-await
+async function generateJoke() {
+        const config = {
+            headers : {
+                "Accept":"application/json"
+            }
+        };
+        const response = await fetch("https://icanhazdadjoke.com",config);
+        const data = await response.json(); //using await since response.json() returns a promise
         jokeElement.innerHTML = data.joke;
-    } );
-}
+    }
