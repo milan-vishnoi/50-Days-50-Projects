7774df4a21c4b219b973c4638b9163b79c65e3de
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-10 20:45:49 +0530

    Added function to do random selection and highlight and unhighlight the tags.

--- 13. Random Choice Picker/script.js
+++ 13. Random Choice Picker/script.js
@@ -5,7 +5,38 @@ textarea.focus();
 
 textarea.addEventListener("keyup",(e) => {
     createTag(e.target.value);
+
+    if(e.key === "Enter")
+    { 
+        setTimeout(()=> {
+            e.target.value = '';
+        },10)
+        randomSelect();
+    }
 });
+
+function randomSelect() {
+    const times = 30;
+    //This interval is for random highlighting and unhighlighting
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unhighlightTag(randomTag);
+        }, 300);
+    }, 300);
+
+    //This timeout is for picking the random tag and highlighting it
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 200);
+    }, times * 100);
+}
 
 function createTag(input) {
    const tags = input.split(",").filter(tag => tag.trim() !== "")
@@ -18,6 +49,18 @@ function createTag(input) {
         tagEl.innerHTML = tag;
         tagsEl.appendChild(tagEl);
     })
-    console.log(tags);
+
 }
 
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag");
+    return tags[Math.floor(Math.random()*tags.length)];
+}
+
+function highlightTag(randomTag) {
+    randomTag.classList.add("highlight");
+}
+
+function unhighlightTag(randomTag) {
+    randomTag.classList.remove("highlight");
+}
