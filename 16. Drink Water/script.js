0650e82a8867526945d4495c68636d91d4c0875b
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-13 20:29:42 +0530

    Added code for handling filling of big cup and updating the filled percentage

--- 16. Drink Water/script.js
+++ 16. Drink Water/script.js
@@ -1,5 +1,9 @@
 const smallCups = document.querySelectorAll(".cup-small");
+const remained = document.getElementById("remained");
+const litres = document.getElementById("Litres");
+const percentage = document.getElementById("percentage");
 
+updateBigCup();
 
 smallCups.forEach((cup,index)=> {
     cup.addEventListener("click",()=> highlightCup(index))
@@ -22,4 +26,23 @@ function highlightCup(index) {
             cup.classList.remove("full");
         }
     });
+
+    updateBigCup();
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll(".cup-small.full").length;
+    const totalCups = document.querySelectorAll(".cup-small").length;
+
+    if( fullCups ===0 )
+    {
+        percentage.style.visibility = "hidden";
+        percentage.style.height = 0;
+    } 
+    else
+    {
+        percentage.style.visibility = "visible";
+        percentage.style.height = `${fullCups/totalCups * 330}px`;
+        percentage.innerText = `${fullCups/totalCups * 100}%`
+    }
 }
