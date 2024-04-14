4b79e103f72b95bad7f62e971fa1e4f519d0a88a
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-14 19:51:59 +0530

    Added comments for the API Key

--- 17. Movie App/script.js
+++ 17. Movie App/script.js
@@ -1,4 +1,5 @@
-const API_KEY = <key Here>;
+//Get your API key from https://developer.themoviedb.org/
+const API_KEY = //replace your key here;
 const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="+API_KEY+"&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key="+API_KEY+"&query=";
