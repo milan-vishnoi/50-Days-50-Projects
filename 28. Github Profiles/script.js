9819b14f2352f14ef14979bade451c4e764dd306
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-27 19:54:39 +0530

    Moved the card content from html to js code in order to set data dynamically.

--- 28. Github Profiles/script.js
+++ 28. Github Profiles/script.js
@@ -1,5 +1,6 @@
 const APIURL = "https://api.github.com/users/";
 
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
@@ -7,10 +8,37 @@ async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username);
 
-    console.log(data);
+    createCardOfUser(data);
   } catch (error) {
     console.log(error);
   }
+}
+
+function createCardOfUser(userData){
+    const card = `
+    <div class="card">
+            <div>
+                <img src="${userData.avatar_url}" alt="${userData.name}" class="avatar">
+            </div>
+            <div class="user-info">
+                <h2>${userData.name}</h2>
+                <p>${userData.bio}</p>
+                <ul>
+                    <li>${userData.followers} <strong>Followers</strong></li>
+                    <li>${userData.following} <strong>Following</strong></li>
+                    <li>${userData.public_repos} <strong>Repos</strong></li>
+                </ul>
+
+                <div class="repos">
+                    <a href="#" class="repo">Repo 1</a>
+                    <a href="#" class="repo">Repo 2</a>
+                    <a href="#" class="repo">Repo 3</a>
+                </div>
+            </div>
+        </div>
+    `;
+
+    main.innerHTML = card ;
 }
 
 form.addEventListener("submit",(e)=> {
