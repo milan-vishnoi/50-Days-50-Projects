f95a54d8b66c8af3b8ac996230a7e2142931c4af
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-30 20:56:58 +0530

    Added function to generate the password and display the results on the screen.

--- 31. Password Generator/script.js
+++ 31. Password Generator/script.js
@@ -5,6 +5,7 @@ const uppercaseElement = document.getElementById("uppercase");
 const numberElement = document.getElementById("numbers");
 const symbolElement = document.getElementById("symbols");
 const generateElement = document.getElementById("generate");
+const clipboardElement = document.getElementById("clipboard");
 
 const randomFunction = {
 lower : getRandomLowercase,
@@ -13,7 +14,36 @@ number : getRandomNumber,
 symbol : getRandomSymbol
 };
 
+generateElement.addEventListener("click", () => {
+    const length = +lengthElement.value;
+    const hasUpper = uppercaseElement.checked;
+    const hasLower = lowercaseElement.checked;
+    const hasNumber = numberElement.checked;
+    const hasSymbol = symbolElement.checked;
 
+    resultElement.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(upper, lower, number, symbol, length) {
+    let password = "";
+    let typesCount = upper + lower + number + symbol;
+    let typesArray = [{upper},{lower},{number},{symbol}].filter(item => Object.values(item)[0]);
+    
+    if(typesCount === 0)
+    {
+        return "";
+    }
+    
+    for(let i=0; i<length;i+=typesCount)
+    {
+        typesArray.forEach(type => {
+              const functionName = Object.keys(type)[0];
+              password+= randomFunction[functionName]();
+        })
+    }
+     
+    return password.slice(0,length);
+}
 
 function getRandomLowercase() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -31,5 +61,3 @@ function getRandomSymbol() {
     const symbols = "!@#$%^&*(){}[]=<>,./";
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-console.log(getRandomLowercase()+" "+getRandomUppercase()+" "+getRandomNumber()+" "+getRandomSymbol());
