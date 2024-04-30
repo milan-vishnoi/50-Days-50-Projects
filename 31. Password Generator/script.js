c6e7ab67907f975b87a6f02e2d097f44e7962307
Milan Vishnoi <milan.vish10@gmail.com>
2024-04-30 20:31:12 +0530

    Imported the elements from dom to the variables

--- 31. Password Generator/script.js
+++ 31. Password Generator/script.js
@@ -1,9 +1,19 @@
+const resultElement = document.getElementById("result");
+const lengthElement = document.getElementById("length");
+const lowercaseElement = document.getElementById("lowercase");
+const uppercaseElement = document.getElementById("uppercase");
+const numberElement = document.getElementById("numbers");
+const symbolElement = document.getElementById("symbols");
+const generateElement = document.getElementById("generate");
+
 const randomFunction = {
 lower : getRandomLowercase,
 upper : getRandomUppercase,
 number : getRandomNumber,
 symbol : getRandomSymbol
 };
+
+
 
 function getRandomLowercase() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
