6c88877a14d04644463d878e0e9c452ab30dac72
Milan Vishnoi <milan.vish10@gmail.com>
2024-05-03 20:51:52 +0530

    Added code to run animation and also reset the dom

---
+++ 34. Animated Countdown/script.js
@@ -0,0 +1,32 @@
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const final = document.querySelector(".final");
+const replayButton = document.querySelector("#replay");
+
+runAnimation();
+
+function runAnimation(){
+    nums.forEach((num,index)=> {
+        const last = nums.length - 1;
+        num.addEventListener("animationend",(event)=>{
+            if(event.animationName === "goIn" && index !== last) {
+                    num.classList.remove("in");
+                    num.classList.add("out");
+            }else if(event.animationName === "goOut" && num.nextElementSibling) {
+                num.nextElementSibling.classList.add("in");
+            }else {
+                 counter.classList.add("hide");
+                 final.classList.add("show");
+            }
+        });
+    });
+}
+
+function resetDOM() {
+    counter.classList.remove("hide");
+    final.classList.remove("show");
+
+    nums.forEach(num => num.classList.value = "") ;
+
+    nums[0].classList.add("in");
+}
