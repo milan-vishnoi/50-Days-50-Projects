4d1787eb3f212f439e72f75ea6ea4759bfe9aea4
Milan Vishnoi <milan.vish10@gmail.com>
2024-05-06 20:56:20 +0530

    Added code to display the pokemon card with details and appropriate colors

--- 37. Pokedex/script.js
+++ 37. Pokedex/script.js
@@ -1,4 +1,4 @@
-const poke_container = document.getElementById("poke_container");
+const poke_container = document.getElementById("poke-container");
 const pokemon_count = 150;
 
 const colors = {
@@ -17,6 +17,8 @@ flying: "#F5F5F5",
 fighting: "#E6E0D4",
 normal: "#F5F5F5" };
 
+const typesArray = Object.keys(colors);
+
 const fetchPoekmons = async () => {
     for(let i = 1 ; i <= pokemon_count ; i++)
         {
@@ -28,7 +30,33 @@ const getPokemon = async (id) => {
      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
      const response = await fetch(url);
      const data = await response.json();
-     console.log(data);
+     displayPokemonCard(data);
 };
 
 fetchPoekmons();
+
+function displayPokemonCard(pokemon)
+{
+    const pokemonElement = document.createElement("div");
+    pokemonElement.classList.add("pokemon");
+    const id = pokemon.id.toString().padStart(3,"0");
+    const pokemonTypes = pokemon.types.map(type => type.type.name);
+    const type = typesArray.find(type => pokemonTypes.indexOf(type) > -1);
+    const color = colors[type];
+    pokemonElement.style.backgroundColor = color;
+
+    pokemonElement.innerHTML = `
+    <div class="pokemon" style="background-color: rgb(222, 253, 224);">
+    <div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${pokemon.name}</h3>
+        <small class="type">Type: <span>${type}</span></small>
+    </div>
+</div>
+    `;
+    poke_container.appendChild(pokemonElement); 
+
+}
