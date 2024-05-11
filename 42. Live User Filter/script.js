635f5584d242263a53df98e34e97dd39c9677637
Milan Vishnoi <milan.vish10@gmail.com>
2024-05-11 19:53:21 +0530

    Added code to getUsers and filter based on the search text

---
+++ 42. Live User Filter/script.js
@@ -0,0 +1,38 @@
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
+const listItems = [];
+
+getUsers();
+
+filter.addEventListener("input" , (event) => filterUsers(event.target.value));
+
+async function getUsers() {
+    const response = await fetch("https://randomuser.me/api?results=1000");
+    const data = await response.json();
+    
+    //Clearing the result screen
+    result.innerHTML = "";
+
+    data.results.forEach(user => {
+         const listItem = document.createElement("li");
+         listItems.push(listItem);
+         listItem.innerHTML = `
+         <img src=${user.picture.large} alt=${user.name.first}>
+         <div class="user-info">
+             <h4>${user.name.first} ${user.name.last}</h4>
+             <p>${user.location.city}, ${user.location.country}</p>
+         </div>
+         `;
+         result.appendChild(listItem);
+    });
+}
+
+function filterUsers(searchText) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchText.toLowerCase())) {
+            item.classList.remove("hide");
+        } else {
+            item.classList.add("hide");
+        }
+    });
+}
