c6e6034aebdc1ab7c15bcd7da284fc5b6025e1c5
Milan Vishnoi <milan.vish10@gmail.com>
2024-05-17 19:41:28 +0530

    Added code to set the height and width according to the number of rows and columns.

--- 48. Random Image Feed/script.js
+++ 48. Random Image Feed/script.js
@@ -1,10 +1,11 @@
-const container = document.querySelector(".image-container");
+const imgContainer = document.querySelector(".image-container");
 const unsplashUrl = "https://source.unsplash.com/random/";
 const rows = document.getElementById("rows");
 const columns = document.getElementById("columns");
 const button = document.getElementById("getImages");
 
 button.addEventListener("click",()=> {
+    imgContainer.innerHTML ="";
     for(let i =0;i<rows.value*columns.value;i++)
         {
               setImages();
@@ -15,8 +16,11 @@ button.addEventListener("click",()=> {
 
 function setImages() {
     const img = document.createElement("img");
+    const imgSize = Math.floor((1000-(20*columns.value))/columns.value);
     img.src = `${unsplashUrl}${getRandomNumber()}x${getRandomNumber()}`;
-    container.appendChild(img);
+    img.style.width=`${imgSize}px`;
+    img.style.height=`${imgSize}px`;
+    imgContainer.appendChild(img);
 }
 
 
